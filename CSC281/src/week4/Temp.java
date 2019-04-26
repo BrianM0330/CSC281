2b07d01f0cd02b27f217de186558ba30b58a0a87
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-04-26 16:19:06 -0500

    3/3 ready to submit
    small bug in the run where the first doubles are 0.0 = 0.0

--- CSC281/src/week4/Temp.java
+++ CSC281/src/week4/Temp.java
@@ -1,68 +1,90 @@
 package week4;
 
-//Put your name here
-//Put your collaboration statement here
+//Brian Mendoza 
+//I did not collaborate with anybody for this assignment. I worked alone. 
 
 import java.util.Scanner;
 
-public class Temp
-{
-	// Please do not declare any additional members of the class
-	// Feel free to use local variables as necessary in each method
+public class Temp {
 	private double tValue;
-	// If you like, you can change the type of scale to Character
+
 	private Character scale;
-	
+
 	private Scanner vScan = new Scanner(System.in);
 
- 	// The default constructor for the class
- 	public Temp()
- 	{
-     	double tempF = 32.0;
-     	double tempC = 0.0;
- 	}
+	public Temp() {
+		this.tValue = 32.0;
+		this.scale = 'f';
+	}
 
- 	// The parameterized constructor for the class
-	public Temp(double initT, Character initS) //This is the one used in lines 12-15 in the driver
-	{
-		this.scale = initS; //Declares the scale to be uased as initS
-		this.tValue = initT; //Declares the temperature to be used as initT
+	public Temp(double initT, Character initS) {
+		this.scale = initS; // Declares the scale to be uased as initS
+		this.tValue = initT; // Declares the temperature to be used as initT
 
-		if ( Character.toLowerCase(initS) == 'c' ) {
+		if (Character.toLowerCase(initS) == 'c') { // Forces the parameter to be lowercase so I can check since there is
+													// no equalIgnoreCase for Characters
 			scale = initS;
 		}
-		else if ( Character.toLowerCase(initS) == 'f') {
+
+		else if (Character.toLowerCase(initS) == 'f') {
 			scale = initS;
 		}
+
 		else {
 			scale = 'f';
 		}
 	}
-	
- 	// Input values for the instance variables using the Scanner vScan
- 	public void set()
- 	{
-		// You can omit this statement if you wish
-		// It's designed to help you figure out what belongs in this method
- 		System.out.println("Entering the set method.");
- 		
-		// You can omit this statement if you wish
-		// It's designed to help you figure out what belongs in this method
-		System.out.println("Leaving the set method.");
- 	}
 
- 	// Return the temperature in Celsius
-	public double getC()
-	{
-		// A stub -- remove when you write this method
-		return 0.0;
+	public void set() {
+		boolean done = false;
+		System.out.println("Entering the set method.");
+
+		// I got this from your example and liked it.
+		// Is it okay if I use this type of structure in future problems? Or is it
+		// inefficient/sub-optimal/a crutch
+
+		while (!done) {
+			System.out.println("Please enter the temp scale (c/f): ");
+			String charS = vScan.nextLine();
+
+			if (charS.equalsIgnoreCase("C")) {
+				this.scale = charS.charAt(0);
+				done = true;
+			}
+
+			else if (charS.equalsIgnoreCase("F")) {
+				this.scale = charS.charAt(0);
+				done = true;
+			}
+
+			else {
+				System.out.println("Celcius and Fahrenheit are the only valid scales.");
+			}
+		}
+		System.out.println("Please enter a temperature: ");
+		this.tValue = vScan.nextDouble();
+		System.out.println("Leaving the set method.");
 	}
 
-	// Return the temperature in Fahrenheit
-	public double getF()
-	{
-		// A stub -- remove when you write this method
-		return 0.0;
+	public double getC() {
+		if (Character.toLowerCase(scale) == 'c') {
+			return tValue;
+		}
+
+		else {
+			this.tValue = (tValue - 32) / 1.8;
+			return tValue;
+		}
+	}
+
+	public double getF() {
+		if (Character.toLowerCase(scale) == 'f') {
+			return tValue;
+		}
+
+		else {
+			this.tValue = (tValue * 1.8) + 32;
+			return tValue;
+		}
 	}
 }
-
