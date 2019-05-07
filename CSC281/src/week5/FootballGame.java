211edf05d373157161576f620fae0f8a3bde6c1a
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-05-06 23:12:05 -0500

    problem completed

--- CSC281/src/week5/FootballGame.java
+++ CSC281/src/week5/FootballGame.java
@@ -1,58 +1,63 @@
 package week5;
 
 public class FootballGame {
-	
-	private String team1, team2;
-	private int team1Score, team2Score;
-	
-	private final int MIN_SCORE = 0;
-	
-	private static int totalGames = 0;
-	private static int totalScore = 0;
-	
-	public FootballGame() {
-		this.team1 = "Lane Tech";
-		this.team2 = "Walter Payton";
 
-		team1Score = MIN_SCORE;
-		team2Score = MIN_SCORE;
+    private String team1, team2;
+    private int team1Score, team2Score;
 
-		totalGames = totalGames +1;
-	}
-	
-	public FootballGame(String t1, String t2) {
-		this.team1 = t1;
-		this.team2 = t2;
+    private final int MIN_SCORE = 0;
 
-		team1Score = MIN_SCORE;
-		team2Score = MIN_SCORE;
+    private static int totalGames = 0;
+    private static int totalScore = 0;
 
-		totalGames = totalGames +1;
-	}
-	
-	public String getTeam1() {
-		return team1;
-	}
-	
-	public String getTeam2() {
-		return team2;
-	}
-	
-	public boolean incrScore(String team, int score) {
-		if (score == 1 || score == 2 || score == 3 || score == 6) {
-			if (team.equals(team1)) {
-				team1Score = team1Score + score;
-				totalScore = totalScore + score;
-				return true;
-			} else {
-				team2Score = team2Score + score;
-				totalScore = totalScore + score;
-				return true;
-			}
-		} else {
-			return false;
-		}
-	}
+    public FootballGame() {
+        this.team1 = "Lane Tech";
+        this.team2 = "Walter Payton";
+
+        team1Score = MIN_SCORE;
+        team2Score = MIN_SCORE;
+
+        totalGames = totalGames + 1;
+    }
+
+    public FootballGame(String t1, String t2) {
+        this.team1 = t1;
+        this.team2 = t2;
+
+        team1Score = MIN_SCORE;
+        team2Score = MIN_SCORE;
+
+        totalGames = totalGames + 1;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public boolean incrScore(String team, int score) {
+        if (score == 1 || score == 2 || score == 3 || score == 6) {
+            if (team.equals(team1)) {
+                team1Score = team1Score + score;
+                totalScore = totalScore + score;
+                return true;
+            }
+            else if (team.equals(team2)) {
+                team2Score = team2Score + score;
+                totalScore = totalScore + score;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
 	
 	public void display() {
 		System.out.println("The current score is: " + team1 + ": " + team1Score + ", " + team2 + ": " + team2Score);
@@ -60,7 +65,10 @@ public class FootballGame {
 	
 	public static double getAvg() {
 		if (totalGames > 0) {
-			return totalScore/totalGames;
+		    double totalS = (double) totalScore;
+		    double totalG = (double) totalGames;
+		    double average = totalS/totalG;
+		    return average;
 		}
 		else {
 			return 0.0;
