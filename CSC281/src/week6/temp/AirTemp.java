95bddd8975d4f12cc4e8c75abd531136a0724c1e
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-05-10 12:56:06 -0500

    wrote toString and airTemp(params) methods

--- CSC281/src/week6/temp/AirTemp.java
+++ CSC281/src/week6/temp/AirTemp.java
@@ -25,12 +25,41 @@ public class AirTemp extends Temp {
 	}
 	
 	public AirTemp(double initT, char initS) {
-		
+	    if (initS == 'f' || initS == 'c' || initS == 'F' || initS == 'C') {
+	        scale = initS;
+        }
+	    else {
+            scale = 'c';
+        }
+	    if (scale == 'c') {
+            if (initT <= MAXC && initT >= MINC) {
+                tValue = initT;
+                numAir = numAir + 1;
+            }
+            else {
+                tValue = MAXC;
+                numAir = numAir +1;
+            }
+        }
+	    else {
+	        if (initT <= MAXF && initT >= MINF) {
+	            tValue = initT;
+	            numAir = numAir + 1;
+            }
+	        else {
+	            tValue = MAXF;
+	            numAir = numAir + 1;
+            }
+	    }
 	}
 	
 	public String toString() {
-		// A stub -- delete when you write the class
-		return "";
+		if (scale == 'c') {
+		    return String.format("%2f Celsius", tValue);
+        }
+		else {
+		    return String.format("%2f Fahrenheit", tValue);
+        }
 	}
 	
 	public boolean equals(AirTemp other) {
