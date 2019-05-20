214008fc73f66cb846ed420e4dabc776d2043990
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-05-20 11:52:19 -0500

    fully functional

--- CSC281/src/week7/people/Child.java
+++ CSC281/src/week7/people/Child.java
@@ -1,28 +1,29 @@
 package week7.people;
 
 // Brian Mendoza
-// Put your collaboration statement here
+// Worked alone.
 
 import week7.Person;
 
 import java.time.LocalDate;
 
 public class Child extends Person implements Comparable<Child>{
-	
+
 	private String parent;
-	
+
 	private final int AGE_MAJ = 18;
 	private final int D_AGE = 15;
-	
+
 	public Child() {
 		name = "Erin";
 		parent = "Amber Settle";
 		LocalDate today = LocalDate.now();
 		bYear = today.getYear() - D_AGE;
 	}
-	
+
 	public Child(String n, int y, String p) {
-	    name = n; parent = p;
+	    name = n;
+	    parent = p;
 		LocalDate today = LocalDate.now();
 		int theYear = today.getYear();
 
@@ -33,19 +34,19 @@ public class Child extends Person implements Comparable<Child>{
 			bYear = theYear - D_AGE;
 		}
 	}
-	
+
 	public int compareTo(Child other) {
         LocalDate today = LocalDate.now();
         int theYear = today.getYear();
 
-	    if (this.bYear - theYear < other.bYear - theYear)
+	    if (theYear - this.bYear < theYear - other.bYear)
 	        return -1;
-	    else if (this.bYear > other.bYear - theYear)
+	    else if (theYear - this.bYear > theYear - other.bYear)
 	        return 1;
 	    else
 	        return 0;
 	}
-	
+
 	public String toString() {
 		return String.format("Name: %s, birth year: %d, parent: %s", name, bYear, parent);
 	}
