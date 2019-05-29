c8c14ea51b512d0beaae467947ee1eabdea989eb
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-05-29 12:02:06 -0500

    fully functional

--- CSC281/src/week8/NumArrayList.java
+++ CSC281/src/week8/NumArrayList.java
@@ -8,14 +8,13 @@ import java.util.ArrayList;
 public class NumArrayList<T extends Number> {
 
 	private ArrayList<T> L;
-
 	public NumArrayList() {
 		this.L = new ArrayList<>();
 	}
 
 	 //Create a new NumArrayList, initializing it with the parameter
 	public NumArrayList(T[] initValues) {
-		L = new ArrayList<>(); //WHY DID THIS WORK (Creates new ArrayList, but how does it use type T?
+		L = new ArrayList<T>(); //WHY DID THIS WORK (Creates new ArrayList, but how does it use type T?
 		//Basically, I kind of get what type T means but maybe you can explain it better
 		for (T i: initValues) { //for value i type T in the array initValues
 			this.L.add(i); //add that value to the arrayList
@@ -41,7 +40,7 @@ public class NumArrayList<T extends Number> {
 
 
 	// Multiply the NumArrayList by an Integer value
-	public NumArrayList<Integer> iMult(Integer val) { //explain this a little? wtf is iMult?
+	public NumArrayList<Integer> iMult (Integer val) { //explain this a little? wtf is iMult?
 		NumArrayList<Integer> product = new NumArrayList<>(); //the NumArrayList of type integer named product
 		for (T i:L) //for an i type T in the existing arrayList L
 			product.add(  i.intValue()  *val); //add the integer value of i (the current value in L) (could be a double) and then multiply THAT value by val
