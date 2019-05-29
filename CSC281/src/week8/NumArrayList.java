15c41e6053c7525544eb9d0a8e20d8dee2ef6248
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-05-29 12:08:46 -0500

    fully functional

--- CSC281/src/week8/NumArrayList.java
+++ CSC281/src/week8/NumArrayList.java
@@ -1,7 +1,7 @@
-package week8; //!!!!!!!!!!!!!!!!!!!!!! CHANGE BACK TO hw8
+package hw8;
 
 // Brian Mendoza
-// Worked with tutor.
+// Worked with tutor Brandon Meng to help solidify the concept of a 'type T'
 
 import java.util.ArrayList;
 
@@ -12,12 +12,10 @@ public class NumArrayList<T extends Number> {
 		this.L = new ArrayList<>();
 	}
 
-	 //Create a new NumArrayList, initializing it with the parameter
 	public NumArrayList(T[] initValues) {
-		L = new ArrayList<T>(); //WHY DID THIS WORK (Creates new ArrayList, but how does it use type T?
-		//Basically, I kind of get what type T means but maybe you can explain it better
-		for (T i: initValues) { //for value i type T in the array initValues
-			this.L.add(i); //add that value to the arrayList
+		L = new ArrayList<>();
+		for (T i: initValues) {
+			this.L.add(i);
 		}
 	}
 
@@ -29,22 +27,18 @@ public class NumArrayList<T extends Number> {
 		return this.L.get(index);
 	}
 
-	// Return the String representation of the NumArrayList
 	public String toString() {
 		String toPrint = "";
-		for (T s : this.L) //Iterator variable type T named S, going through L.
+		for (T s : this.L)
 			toPrint = toPrint + s.toString() + " ";
 		return toPrint;
 	}
 
-
-
-	// Multiply the NumArrayList by an Integer value
-	public NumArrayList<Integer> iMult (Integer val) { //explain this a little? wtf is iMult?
-		NumArrayList<Integer> product = new NumArrayList<>(); //the NumArrayList of type integer named product
-		for (T i:L) //for an i type T in the existing arrayList L
-			product.add(  i.intValue()  *val); //add the integer value of i (the current value in L) (could be a double) and then multiply THAT value by val
-		return product; //return the numArrayList named product
+	public NumArrayList<Integer> iMult (Integer val) {
+		NumArrayList<Integer> product = new NumArrayList<>();
+		for (T i:L)
+			product.add(  i.intValue()  *val);
+		return product;
 	}
 }
 
