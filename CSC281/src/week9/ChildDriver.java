62808e07a5e937f3a20d6e70e7e1d1d7a9f82731
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-06-04 04:13:04 -0500

    all good

--- CSC281/src/week9/ChildDriver.java
+++ CSC281/src/week9/ChildDriver.java
@@ -1,39 +1,27 @@
-package week9;
-import java.util.Arrays;
-import week9.Child;
+//Brian Mendoza. I worked alone. It is 4AM I am losing my marbles.
 
-public class ChildDriver extends Child implements Comparable <Child> {
+package week9;
+
+import java.util.Arrays;
+
+public class ChildDriver extends Child implements Comparable<Child> {
 
     final static int MAX = 3;
+
     public static void main(String[] args) {
-        //step A
-       Child[] kids = new Child[MAX]; //An array of the type child named childArray is created with the length of MAX
+        Child[] kids = new Child[MAX];
+        kids[0] = new Child();
+        kids[1] = new Child("Joon", 1994, "Jeff Elam");
+        kids[2] = new Child("Prudence", 2007, "Andre Berthiaume");
 
-        //step B
-       kids[0] = new Child();
-       kids[1] = new Child("Joon", 1994, "Jeff Elam");
-       kids[2] = new Child("Prudence", 2007, "Andre Berthiaume");
+        System.out.println("The children: " + Arrays.toString(kids));
 
-       //step C
-       System.out.println("The children: " + Arrays.toString(kids));
+        Arrays.sort(kids);
 
-       //step D
-        Arrays.sort(kids); //already implements comparable with the header. Issue was the MAX amount of entries
-
-        //Step E
         System.out.println("The child Array, after sorting (using compareTo): " + Arrays.toString(kids));
 
-        //step F
         Arrays.sort(kids, (first, second) -> first.getParent().compareTo(second.getParent()));
 
-        //step G
         System.out.println("The child array, after sorting (using the lambda): " + Arrays.toString(kids));
-//            if (first.getParent().compareTo(second.getParent()) > 0)
-//                return 1;
-//            else if (first.getParent().compareTo(second.getParent()) < 0)
-//                return -1;
-//            else return 0;
-
     }
-
 }
